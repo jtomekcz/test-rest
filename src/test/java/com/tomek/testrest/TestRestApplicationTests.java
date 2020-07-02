a9f3a52cf02d36ec4981fee88121da8e97c0aa64
jan.tomek <jan.tomek@eurowag.com>
2020-07-02 10:37:13 +0200

    initial commit

---
+++ src/test/java/com/tomek/testrest/TestRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomek.testrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
