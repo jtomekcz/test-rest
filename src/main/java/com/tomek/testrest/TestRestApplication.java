11d4285442b1c1f9725086ab09ffb9507a53c220
jan.tomek <jan.tomek@eurowag.com>
2020-07-02 11:11:34 +0200

    added web-engine config file

--- src/main/java/com/tomek/testrest/TestRestApplication.java
+++ src/main/java/com/tomek/testrest/TestRestApplication.java
@@ -2,12 +2,23 @@ package com.tomek.testrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TestRestApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TestRestApplication.class, args);
+  }
+
+  @RestController
+  class Controller {
+
+    @GetMapping("/test")
+    public String test() {
+      return "test of rest_test";
+    }
   }
 
 }
